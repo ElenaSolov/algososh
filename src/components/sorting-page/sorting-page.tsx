5f2eda5eddbc8d2b103ea4b4aa464bccd633e474
Elena Soloveva <seaver@yandex.ru>
2022-09-04 14:08:53 +0300

    feat: add main elements to sorting page, add getRandomInt function to utils

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { SyntheticEvent } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import sortingStyles from "./sorting-page.module.css";
+import { Button } from "../ui/button/button";
+import { RadioInput } from "../ui/radio-input/radio-input";
+import { getRandomInt } from "../../utils/utils";
 
 export const SortingPage: React.FC = () => {
+  const onSubmit = (e: SyntheticEvent) => {
+    e.preventDefault();
+  };
+
+  const randomArr = () => {
+    const arrayLength = getRandomInt(3, 17);
+    const array = [];
+    for (let i = 0; i <= arrayLength; i++) {
+      array.push(getRandomInt(0, 100));
+    }
+    console.log(array);
+  };
+  randomArr();
   return (
     <SolutionLayout title="Сортировка массива">
-
+      <form onSubmit={onSubmit} className={sortingStyles.container}>
+        <section className={sortingStyles.radio}>
+          {" "}
+          <RadioInput name="method" label="Выбор" checked />
+          <RadioInput name="method" label="Пузырёк" />
+        </section>
+        <section className={sortingStyles.method}>
+          <Button type="button" text="По возрастанию" />
+          <Button type="button" text="По убыванию" />
+        </section>
+        <Button type="button" text="Новый массив" />
+      </form>
+      <section className={sortingStyles.output}>1</section>
     </SolutionLayout>
   );
 };
