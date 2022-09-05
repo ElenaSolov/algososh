517399a5beafcbcc2697893c10fa9523567c8bef
Elena Soloveva <seaver@yandex.ru>
2022-09-05 13:58:48 +0300

    feat: add bubble sort logic

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,52 +1,156 @@
-import React, { SyntheticEvent, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import sortingStyles from "./sorting-page.module.css";
+import { Direction } from "../../types/direction";
 import { Button } from "../ui/button/button";
 import { RadioInput } from "../ui/radio-input/radio-input";
-import { getRandomInt } from "../../utils/utils";
+import { Column } from "../ui/column/column";
+import { getRandomArr, swap } from "../../utils/utils";
+import { ElementStates } from "../../types/element-states";
 
 export const SortingPage: React.FC = () => {
   const [array, setArray] = useState<number[]>([]);
-  const onSubmit = (e: SyntheticEvent) => {
-    e.preventDefault();
-  };
+  const [method, setMethod] = useState("selection");
+  const [direction, setDirection] = useState(Direction.Ascending);
+  const [done, setDone] = useState(true);
+  const [i, setI] = useState(-2);
+  const [k, setK] = useState(-2);
+  const [sorted, setSorted] = useState(false);
 
-  const getRandomArr = () => {
-    const arrayLength = getRandomInt(3, 17);
-    const array = [];
-    for (let i = 0; i < arrayLength; i++) {
-      array.push(getRandomInt(0, 100));
-    }
-    console.log(array);
-    return array;
-  };
   useEffect(() => {
     setArray(getRandomArr());
   }, []);
+
+  const onValueChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    setMethod(evt.target.value);
+    console.log(method);
+  };
+  const reset = () => {
+    setI(-2);
+    setK(-2);
+    setSorted(false);
+  };
+  useEffect(() => {
+    let setTimer: number | undefined;
+    if (!done) {
+      setTimer = window.setInterval(() => {
+        console.log(1);
+        doStep();
+      }, 1000);
+    }
+    if (done) {
+      window.clearInterval(setTimer);
+    }
+    return () => window.clearInterval(setTimer);
+  }, [done]);
+
+  const onClick = () => {
+    console.log(method);
+    reset();
+    setDone(false);
+    method === 'bubble'
+    ? bubbleSort()
+    : selectionSort();
+  };
+
+  function* bubbleSort() {
+    const { length } = array;
+    let arr = array.slice();
+    console.log(arr);
+    for (let i = 0; i < length - 1; i++) {
+      for (let k = 0; k < length - i - 1; k++) {
+        if (
+          (direction === "ascending" && arr[k] > arr[k + 1]) ||
+          (direction === "descending" && arr[k] < arr[k + 1])
+        ) {
+          swap(arr, k, k + 1);
+          console.log(i, arr, k);
+        }
+        yield { arr, i, k };
+      }
+    }
+    setDone(true);
+    setSorted(true);
+  }
+  const gen = bubbleSort();
+  async function doStep() {
+    const action = await gen.next();
+    if (action.done) {
+      setDone(true);
+      console.log(3);
+    } else {
+      setArray(action.value.arr);
+      setI(action.value.i);
+      setK(action.value.k);
+      console.log(action.value);
+    }
+  }
+  const getState = (index: number) => {
+    if (sorted) return ElementStates.Modified;
+    if (index === k || index === k + 1) {
+      return ElementStates.Changing;
+    } else if (index >= array.length - i) {
+      return ElementStates.Modified;
+    }
+    return ElementStates.Default;
+  };
+
   return (
     <SolutionLayout title="Сортировка массива">
-      <form onSubmit={onSubmit} className={sortingStyles.container}>
+      <div className={sortingStyles.container}>
         <section className={sortingStyles.radio}>
-          <RadioInput name="method" label="Выбор" defaultChecked />
-          <RadioInput name="method" label="Пузырёк" />
+          <RadioInput
+            name="method"
+            label="Выбор"
+            value="selection"
+            checked={method === "selection"}
+            onChange={onValueChange}
+          />
+          <RadioInput
+            name="method"
+            label="Пузырёк"
+            value="bubble"
+            checked={method === "bubble"}
+            onChange={onValueChange}
+          />
         </section>
         <section className={sortingStyles.method}>
-          <Button type="button" text="По возрастанию" />
-          <Button type="button" text="По убыванию" />
+          <Button
+            type="button"
+            text="По возрастанию"
+            sorting={Direction.Ascending}
+            onClick={() => {
+              setDirection(Direction.Ascending);
+              onClick();
+            }}
+          />
+          <Button
+            type="button"
+            text="По убыванию"
+            sorting={Direction.Descending}
+            onClick={() => {
+              setDirection(Direction.Descending);
+
+              onClick();
+            }}
+          />
         </section>
-        <Button type="button" text="Новый массив" />
-      </form>
-      <section className={sortingStyles.output}>
-        {array.map((num, index) => (
-          <div
-            key={index}
-            className={sortingStyles.arrayItem}
-            style={{ height: num }}
-          >
-            {num}
-          </div>
+        <Button
+          type="button"
+          text="Новый массив"
+          onClick={() => {
+            reset();
+            setArray(getRandomArr());
+          }}
+        />
+      </div>
+      <ul className={sortingStyles.output}>
+        {array?.map((el, index) => (
+          <li className={sortingStyles.item} key={index}>
+            <Column index={el} state={getState(index)} />
+          </li>
         ))}
-      </section>
+      </ul>
     </SolutionLayout>
   );
 };
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,27 @@
-export function getRandomInt(min: number, max: number): number {
+function getRandomInt(min: number, max: number): number {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+export const getRandomArr = () => {
+  const arrayLength = getRandomInt(3, 17);
+  const array = [];
+  for (let i = 0; i < arrayLength; i++) {
+    array.push(getRandomInt(0, 100));
+  }
+  console.log(array);
+  return array;
+};
+
+export const swap = (
+  arr: number[],
+  firstIndex: number,
+  secondIndex: number
+): number[] => {
+  const temp = arr[firstIndex];
+  arr[firstIndex] = arr[secondIndex];
+  arr[secondIndex] = temp;
+  return arr;
+};
+
