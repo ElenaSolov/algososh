e80a846df6eedc3338796713daf5c08868014410
Elena Soloveva <seaver@yandex.ru>
2022-10-19 13:37:17 +0300

    feat: add fibonacci page e2e tests

---
+++ cypress/e2e/fibonacci.spec.cy.ts
@@ -0,0 +1,36 @@
+import {
+  circleSelector,
+  defaultColor,
+  inputSelector,
+  submitBtnSelector,
+} from "../support/constants";
+import { DELAY_IN_MS } from "../../src/constants/delays";
+
+describe("Spring page", () => {
+  beforeEach(() => {
+    cy.visit("/fibonacci");
+  });
+
+  it("Button is disabled when input is empty", () => {
+    cy.get(inputSelector).clear();
+    cy.get(submitBtnSelector).should("be.disabled");
+  });
+  it("Fibonacci numbers are generated correctly", () => {
+    const initialInputValue = "4";
+    const fibonacciOrder = [1, 1, 2, 3, 5];
+    cy.clock();
+    cy.get(inputSelector).type(initialInputValue);
+    cy.get(submitBtnSelector).click();
+
+    for (let i = 0; i <= +initialInputValue; i++) {
+      cy.get(circleSelector).then((nums) => {
+        expect(nums).to.have.length(i + 1);
+        cy.wrap(nums)
+          .eq(i)
+          .should("have.css", "border-color", defaultColor)
+          .contains(fibonacciOrder[i]);
+      });
+      cy.tick(DELAY_IN_MS);
+    }
+  });
+});
